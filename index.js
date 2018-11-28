e35c723a44a2ace7d6e7efba763ac68af2ad2947
amdayton <amdayton@gmail.com>
2018-11-28 16:41:01 -0600

    Trying to figure out how to add a shader

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ mapboxgl.accessToken = MAPBOX_API_TOKEN;
 
 const mapEl = document.createElement('div');
 mapEl.id = 'map';
+// mapEl.width = window.innerWidth;
+// mapEl.height = window.innerHeight;
 document.body.append(mapEl);
 
 var lon = -93.282496;
@@ -21,6 +23,59 @@ const map = new mapboxgl.Map({
   zoom: zoom,
   center: new mapboxgl.LngLat(lon ,lat)
 });
+
+map.on('load', () => {
+  const mapCanvas = mapEl.querySelector('canvas');
+  console.log(mapCanvas);
+  const ctx = mapCanvas.getContext('webgl');
+  console.log(ctx);
+})
+
+
+// class NullIslandLayer {
+//   constructor() {
+//     this.id = 'null-island';
+//     this.type = 'custom';
+//     this.renderingMode = '2d';
+//   }
+
+//   onAdd(map, gl) {
+//     const vertexSource = `
+//         uniform mat4 u_matrix;
+//         void main() {
+//             gl_Position = u_matrix * vec4(0.5, 0.5, 0.0, 1.0);
+//             gl_PointSize = 20.0;
+//         }`;
+
+//     const fragmentSource = `
+//         void main() {
+//             gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+//         }`;
+
+//     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+//     gl.shaderSource(vertexShader, vertexSource);
+//     gl.compileShader(vertexShader);
+//     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+//     gl.shaderSource(fragmentShader, fragmentSource);
+//     gl.compileShader(fragmentShader);
+
+//     this.program = gl.createProgram();
+//     gl.attachShader(this.program, vertexShader);
+//     gl.attachShader(this.program, fragmentShader);
+//     gl.linkProgram(this.program);
+//   }
+
+//   render(gl, matrix) {
+//     gl.useProgram(this.program);
+//     gl.uniformMatrix4fv(gl.getUniformLocation(this.program, "u_matrix"), false, matrix);
+//     gl.drawArrays(gl.POINTS, 0, 1);
+//   }
+// }
+
+// map.on('load', function () {
+//   map.addLayer(new NullIslandLayer());
+// });
+
 
 const overlay = document.createElement('canvas');
 overlay.id = 'overlay';
@@ -44,6 +99,7 @@ const drawOverlay = () => {
   ctx.drawImage(mapCanvas, 0, 0)
   window.setTimeout(drawOverlay, 500);
 }
+map.on('load', drawOverlay);
 
 // console.log('tile grabber go!')
 // tileGrabber(
@@ -59,7 +115,6 @@ const drawOverlay = () => {
 
 
 
-map.on('load', drawOverlay);
 
 
 
